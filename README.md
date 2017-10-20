066be73aa71ec646f959153e4798655f09c580e5
multapaucis <matthewrfree@gmail.com>
2017-10-20 13:51:25 -0700

    Update fresh_tomatoes to display tagline and duration of films Add Media.py to create class for movies Add entertainment_center.py to create list of Movies and call fresh_tomatoes to create a website

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Website
+A Movie Trailer website, filled in with 6 movies I enjoy. 
+The Movies display Movie Art, Title, Tagline, and Estimated Duration. 
+Clicking on the movie pulls up a trailer for the film from Youtube.
+
+To open website, open and run the entertainment_center.py file.
+The html file will then be create in the same directory as entertainment_center.py
+The html file will also be automatically opened in the default web browser
+
+
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -9,7 +9,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Matt's Movies!</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Matt's Movies</a>
           </div>
         </div>
       </div>
@@ -124,7 +124,9 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h3>{movie_title}</h3>
+    <p>{tagline}</p>
+    <p>Duration {duration} minutes</p>
 </div>
 '''
 
@@ -145,7 +147,9 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            tagline=movie.storyline,
+            duration= movie.duration
         )
     return content
 
@@ -165,3 +169,5 @@ def open_movies_page(movies):
     # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2)
+
+
